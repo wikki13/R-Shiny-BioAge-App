15442bcc55b15ae914c89fe6dc0e8b4cabc7d3a6
VIGNESHWARA CHANDRASEKARAN <60074197+wikki13@users.noreply.github.com>
2025-04-11 13:44:46 +0530

    Update test_ui_bioage.R

--- test/testthat/test_ui_bioage.R
+++ test/testthat/test_ui_bioage.R
@@ -2,13 +2,13 @@ library(testthat)
 library(shinytest2)
 
 test_that("UI inputs are present and functional", {
-  app <- AppDriver$new(app_dir ="." , seed = 101)
+  app <- AppDriver$new(app_dir = ".", seed = 101)
   
   # Test text inputs
   app$set_inputs(Name = "Wikki")
   app$set_inputs(Surname = "Chan")
   app$set_inputs(phone_prefix = "+91")
-  app$set_inputs(phone_number = "1010101010)
+  app$set_inputs(phone_number = "1010101010")  # fixed quote here
   
   # Test date inputs
   app$set_inputs(dob = "1999-12-12")
@@ -27,9 +27,6 @@ test_that("UI inputs are present and functional", {
   
   # Submit button click
   app$click("submit")
-  
-  # Optionally: you can test reactive value changes or outputs here
-  # app$expect_values() or app$expect_screenshot()
   
   app$stop()
 })
